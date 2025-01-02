7bdb81383cc7c9527e1c3ea801d323890e170159
Aristeidis Palamaras <arpalamaras@hotmail.com>
2025-01-02 11:58:29 +0200

    Initial commit. NoteBoard project set up.

---
+++ src/test/java/gr/aueb/cf/noteboard/NoteBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.noteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
