792a064d18df408408a7cc043bcbd14c5725c0a3
Aristeidis Palamaras <arpalamaras@hotmail.com>
2025-01-31 00:14:22 +0200

    Small modifications to support windows.

--- src/main/resources/sql/demo/messages.sql
+++ src/main/resources/sql/demo/messages.sql
@@ -1,4 +1,4 @@
-USE noteboardDB;
+-- USE noteboardDB;
 
 INSERT INTO `messages` VALUES
                            (1, '2025-01-29 21:10:47.222300', '2025-01-29 21:10:47.222300', 'Sed vulputate faucibus rhoncus. Cras et.', null, 1, 1),
--- src/main/resources/sql/index.sql
+++ src/main/resources/sql/index.sql
@@ -1,4 +1,4 @@
-USE noteboardDB;
+-- USE noteboardDB;
 
 CREATE INDEX idx_users_username ON `users` (username);
 CREATE INDEX idx_groups_name ON `groups` (name);
